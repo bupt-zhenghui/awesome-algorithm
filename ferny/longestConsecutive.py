ba11c0e4cbd873a0f529c2a1c60d819bee0cda3b
zhengxianxian <3455259967@qq.com>
2023-11-24 16:27:26 +0800

    128.最长连续序列

--- ferny/longestConsecutive.py
+++ ferny/longestConsecutive.py
@@ -1,29 +1,45 @@
 class Solution:
     def longestConsecutive(self, nums) -> int:
+        # 用哈希表存储每个端点对应的连续区间的长度
+        # 当数不在哈希表中时，取比它小1的左边的连续数组的长度和比它大1的连续数组的长度。
+        # 注意要对左边数组的长度和右边数组的长度进行更新
         if len(nums) == 0:
             return 0
-        num_set = set(nums)
-        new_set = set()
-        max_length = 0
-        length = 1
-        i = 0
-        while i < len(nums):
-            tmp = nums[i]
-            if tmp not in new_set:
-                while length < len(nums):
-                    if tmp + 1 in num_set:
-                        new_set.add(tmp)
-                        tmp += 1
-                        length += 1
-                    else:
-                        new_set.add(tmp)
-                        break
-            # print(new_set)
-            max_length = max(max_length, length)
-            length = 1
-            # print(f"max_length: {max_length}, length: {length}")
-            i = i + 1
+        dic = {}
+        max_length = 1
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                left = dic.get(nums[i]-1, 0)
+                right = dic.get(nums[i] + 1, 0)
+                cur = left + right + 1
+                dic[nums[i]] = cur
 
+                max_length = max(max_length,cur)
+                # 接下来要更新区间两端点的长度值
+                dic[nums[i] - left] = cur
+                dic[nums[i] + right] = cur
         return max_length
 
-print(Solution().longestConsecutive([100,99,98,0,3,4,5,6]))
+print(Solution().longestConsecutive([1,2,0,1]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(Solution().longestConsecutive([100,99,98,0,3,4,5,6]))
