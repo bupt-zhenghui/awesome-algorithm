a06671168005420e60c398ea44f36d2dc649526a
zhengxianxian <3455259967@qq.com>
2024-01-31 17:06:34 +0800

    739. 堆栈：每日温度

--- ferny/dailyTemperature.py
+++ ferny/dailyTemperature.py
@@ -1,28 +1,35 @@
 class Solution:
     def dailyTemperatures(self, temperatures):
-        n = len(temperatures)
-        idx = 1
+        if len(temperatures) == 1: return [0]
         stack = []
-        stack.append(0)
-        ans = [0]*n
-        idxpre = 0
-        while idx < n:
-            if temperatures[idx] > temperatures[idxpre]:
-                ans[idxpre] = idx - idxpre
+        ans = [0]*len(temperatures)
+        stack.append((temperatures[0],0))
+        i = 1
+        while i <= len(temperatures)-1:
+            x,y = stack[-1]
+            if temperatures[i] > x:
                 stack.pop()
-                if stack:
-                    idxpre = stack[-1]
-                    while stack:
-                        if temperatures[idx] > temperatures[idxpre]:
-                            ans[idxpre] = idx - idxpre
-                            stack.pop()
-                            idxpre -= 1
-                        else:
-                            break
-            stack.append(idx)
-            idx += 1
-            idxpre = stack[-1]
+                # ans.append(i-y)
+                ans[y] = i-y
+                # 注意弹出的气温要放到对应的位置上
+                # 序号直接相减表示间隔的天数
+                while stack:
+                    x1,y1 = stack[-1]
+                    if temperatures[i] > x1:
+                        stack.pop()
+                        # ans.append(i-y1)
+                        ans[y1] = i - y1
+                    else:
+                        break
+                stack.append((temperatures[i],i))
+            else:
+                stack.append((temperatures[i],i))
+                # 在栈里放着的是(每日气温，在数组中对应的序号)
+            i = i + 1
         return ans
+
+
+
 
 print(Solution().dailyTemperatures([73,74,75,71,69,72,76,73]))
 
